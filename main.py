8d80ff3819e7cd696a381f624c7a3028773ec2a8
Oleh Tarnavchuk <olegtar25@gmail.com>
2022-04-18 13:16:37 +0300

    proba pera

--- main.py
+++ main.py
@@ -14,5 +14,5 @@ for i in range(5):
 if __name__ == '__main__':
     print_hi('PyCharm')
 print(gsfdsg)
-
+Not forget to pull the commit
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
